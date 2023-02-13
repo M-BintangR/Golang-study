540668d6c9384f392bf970227384016c81e8c2a6
= <muhbinrang650@gmail.com>
2023-02-13 09:00:56 +0800

    Belajar tipe data string

---
+++ latihan4/functionString.go
@@ -0,0 +1,14 @@
+//? function untuk string
+/*
+	1. len("string")	 => mengambil jumlah karakter di string
+	2. "string"[number]  => mengambil posisi pada posisi yang di tentukan
+*/
+
+package main
+import "fmt"
+
+func main(){
+	fmt.Println(len("Bintang")); // mengambil jumlah karakter
+	fmt.Println("Muh Bintang Ramli"[0]); // mengambil karakter pada i 0
+	fmt.Println("Muh Bintang Ramli"[1]); // mengambil karakter pada i 1
+}
---
+++ latihan4/string.go
@@ -0,0 +1,21 @@
+//? Tipe data string
+/*
+	Jumlah karakter mulai dari 0 ("") hingga tak terhingga
+	Kata kunci nya string
+	di awali dengan tanda (petik dua) " dan di akhiri tanda " (petik dua)
+*/
+
+package main
+import "fmt"
+
+func main(){
+	fmt.Println("Muh");
+	fmt.Println("Muh Bintang");
+	fmt.Println("Muh Ramli");
+}
+
+
+
+
+
+
