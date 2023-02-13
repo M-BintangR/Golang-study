09987920df08711058033670bae18503bdfc6ff3
= <muhbinrang650@gmail.com>
2023-02-13 08:43:47 +0800

    Belajar tipe data bolean

---
+++ latihan3/bolean.go
@@ -0,0 +1,14 @@
+//? Tipe data bolean 
+// ditulis dengan (bool)
+/*
+	1. True = Benar
+	2. False = Salah
+*/
+
+package main
+import "fmt"
+
+func main(){
+	fmt.Println("Benar =", true);
+	fmt.Println("Salah =", false);
+}
